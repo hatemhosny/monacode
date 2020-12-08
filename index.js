b256a47a3cdecca3f93dc1c3b07d7b79cb18ed6b
Hatem Hosny <hatemhosny@gmail.com>
2020-12-08 15:06:41 +0200

    fix formatting

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-import * as monaco from "./src/monaco/editor/editor.main.js";
+import * as monaco from './src/monaco/editor/editor.main.js';
 
-import prettier from "./src/prettier.js";
-import prettierBabel from "./src/prettier-babel.js";
-import prettierHtml from "./src/prettier-html.js";
+import prettier from './src/prettier.js';
+import prettierBabel from './src/prettier-babel.js';
+import prettierHtml from './src/prettier-html.js';
 
-const sheet = document.createElement("style");
+const sheet = document.createElement('style');
 document.head.appendChild(sheet);
 
-sheet.innerHTML = ".monaco-editor { display: none; }";
+sheet.innerHTML = '.monaco-editor { display: none; }';
 
-fetch("./src/index.css")
+fetch('./src/index.css')
   .then((res) => res.text())
   .then((styles) => (sheet.innerHTML = styles));
 
 self.MonacoEnvironment = {
   getWorkerUrl: function (moduleId, label) {
-    if (label === "json") return "./src/monaco/language/json/json.worker.js";
-    if (label === "css") return "./src/monaco/language/css/css.worker.js";
-    if (label === "html") return "./src/monaco/language/html/html.worker.js";
-    if (label === "typescript" || label === "javascript")
-      return "./src/monaco/language/typescript/ts.worker.js";
-    return "./src/monaco/editor/editor.worker.js";
+    if (label === 'json') return './src/monaco/language/json/json.worker.js';
+    if (label === 'css') return './src/monaco/language/css/css.worker.js';
+    if (label === 'html') return './src/monaco/language/html/html.worker.js';
+    if (label === 'typescript' || label === 'javascript')
+      return './src/monaco/language/typescript/ts.worker.js';
+    return './src/monaco/editor/editor.worker.js';
   },
 };
 
@@ -30,7 +30,7 @@ const computeOffset = (code, pos) => {
   let offset = 0;
   while (offset < code.length) {
     if (line === pos.lineNumber && col === pos.column) return offset;
-    if (code[offset] === "\n") line++, (col = 1);
+    if (code[offset] === '\n') line++, (col = 1);
     else col++;
     offset++;
   }
@@ -42,7 +42,7 @@ const computePosition = (code, offset) => {
   let col = 1;
   let char = 0;
   while (char < offset) {
-    if (code[char] === "\n") line++, (col = 1);
+    if (code[char] === '\n') line++, (col = 1);
     else col++;
     char++;
   }
@@ -50,9 +50,9 @@ const computePosition = (code, offset) => {
 };
 
 const editorDefaults = {
-  value: "",
-  language: "typescript",
-  theme: "vs-dark",
+  value: '',
+  language: 'typescript',
+  theme: 'vs-dark',
   formatOnType: false,
   fontSize: 16,
   tabSize: 2,
@@ -73,27 +73,26 @@ export default (options) => {
     ...editorDefaults,
     ...restOfOptions,
   });
-
   const language = editor.getModel().getModeId();
 
   // Import themes directly from the amazing collection by @brijeshb42
   // https://raw.githubusercontent.com/brijeshb42/monaco-themes/master/themes
 
-  if (options.theme === "vs-light") container.style.backgroundColor = "#fff";
-  if (options.theme?.startsWith("http") || options.theme?.startsWith("./"))
+  if (options.theme === 'vs-light') container.style.backgroundColor = '#fff';
+  if (options.theme?.startsWith('http') || options.theme?.startsWith('./'))
     fetch(options.theme)
       .then((res) => res.json())
       .then((data) => {
-        monaco.editor.defineTheme("theme", data);
-        monaco.editor.setTheme("theme");
-        container.style.backgroundColor = data.colors["editor.background"];
+        monaco.editor.defineTheme('theme', data);
+        monaco.editor.setTheme('theme');
+        container.style.backgroundColor = data.colors['editor.background'];
       });
 
-  addEventListener("resize", function () {
+  addEventListener('resize', function () {
     editor.layout();
   });
 
-  const alt = (e) => (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey);
+  const alt = (e) => (navigator.platform.match('Mac') ? e.metaKey : e.ctrlKey);
   const hotKeys = (e) => {
     // Cdm + s formats with prettier
     if (alt(e) && e.keyCode == 83) {
@@ -102,23 +101,23 @@ export default (options) => {
       const pos = editor.getPosition();
 
       const prettyVal = prettier.formatWithCursor(val, {
-        parser: language === "html" ? "html" : "babel",
-        plugins: [language === "html" ? prettierHtml : prettierBabel],
+        parser: language === 'html' ? 'html' : 'babel',
+        plugins: [language === 'html' ? prettierHtml : prettierBabel],
         cursorOffset: computeOffset(val, pos),
       });
 
-      editor.executeEdits("prettier", [
+      editor.executeEdits('prettier', [
         {
-          identifier: "delete",
+          identifier: 'delete',
           range: editor.getModel().getFullModelRange(),
-          text: "",
+          text: '',
           forceMoveMarkers: true,
         },
       ]);
 
-      editor.executeEdits("prettier", [
+      editor.executeEdits('prettier', [
         {
-          identifier: "insert",
+          identifier: 'insert',
           range: new monaco.Range(1, 1, 1, 1),
           text: prettyVal.formatted,
           forceMoveMarkers: true,
@@ -132,7 +131,7 @@ export default (options) => {
     }
     // Cmd + p opens the command palette
     if (alt(e) && e.keyCode == 80) {
-      editor.trigger("anyString", "editor.action.quickCommand");
+      editor.trigger('anyString', 'editor.action.quickCommand');
       e.preventDefault();
     }
     // Cmd + d prevents browser bookmark dialog
@@ -141,7 +140,7 @@ export default (options) => {
     }
   };
 
-  container.addEventListener("keydown", hotKeys);
+  container.addEventListener('keydown', hotKeys);
 
   return editor;
 };
